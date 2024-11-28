b929dd9027a08e8f41f4882412f28baf633cf041
rbmpsh <rbmpsh@gmail.com>
2024-11-28 14:25:09 +0800

    update: use english

--- src/assets/types.ts
+++ src/assets/types.ts
@@ -1,22 +1,15 @@
 export type Discussion = {
   teamId: string;
   nameZh: string;
-  nameEn: string;
-  established: string;
   capital: number;
-  type: string;
-  aum: number;
-  fundSize: number;
   investmentFields: string[];
   fundingRounds: string[];
-  contact: { nameZh: string; nameEn: string; occupationZh: string; occupationEn: string; tel: string; email: string };
+  contact: { nameZh: string; occupationZh: string; tel: string; email: string };
 };
 
 export type DiscussionFormValue = Omit<Discussion, "contact" | "teamId"> & {
   contactNameZh: string;
-  contactNameEn: string;
   contactOccupationZh: string;
-  contactOccupationEn: string;
   contactTel: string;
   contactEmail: string;
 };
